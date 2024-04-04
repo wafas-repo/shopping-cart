5029c01b7fef5e6e4218e0b6d3278a2059f0af88
wafas-repo <wafa.qazi@outlook.com>
2024-04-04 13:35:14 -0400

    sidebar design complete

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React, {useState, createContext, useContext} from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../Sidebar/Sidebar.module.css'
+import { IoMdClose } from "react-icons/io";
+import { IoMdAdd } from "react-icons/io";
 
 import { SidebarContext } from '../../contexts/SidebarContext'
 
@@ -8,9 +10,35 @@ const Sidebar = () => {
   const {isOpen, setIsOpen,  handleClose} = useContext(SidebarContext)
   return (
     <div  className={`${styles.Sidebar}`} style={{ left: isOpen ? '0' : '-100%' }}>
-        <div onClick={() => setIsOpen(!isOpen)}>
-            Close
+        <div className={styles.sidebarTop}>
+          <h6 className={styles.sidebarTitle}>MENU</h6>
+          <div className={styles.sidebarIcon} onClick={() => setIsOpen(!isOpen)}>
+            <IoMdClose />
+          </div>
         </div>
+        <div className={styles.sidebarCenter}>
+          <nav>
+            <li style={{listStyle: 'none'}} >
+              <div className={styles.navItem}>
+                WOMEN
+                <IoMdAdd className={styles.navItemIcon} />
+              </div>
+            </li>
+            <li style={{listStyle: 'none'}} > 
+              <div className={styles.navItem}>
+                  MEN
+                  <IoMdAdd className={styles.navItemIcon} />
+                </div>
+            </li>
+            <li style={{listStyle: 'none'}} >
+            <div className={styles.navItem}>
+                KIDS
+                <IoMdAdd className={styles.navItemIcon} />
+              </div>
+            </li>
+          </nav>
+        </div>
+        
     </div>
   )
 }
