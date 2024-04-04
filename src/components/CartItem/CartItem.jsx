8e5ec4b265d3b4b08ec6e5e42c940338bc8df667
wafas-repo <wafa.qazi@outlook.com>
2024-04-04 01:20:47 -0400

    Created cart page

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 const CartItem = ({item}) => {
+  const {id, title, image, price, amount} = item;
   return (
-    <div>{item.title}</div>
+    <>
+
+      <div className="cart-product">
+            <img src={image} alt={title} />
+            <div>
+              <h3>{title}</h3>
+              <button>Remove</button>
+            </div>
+      </div>
+      <div className="cart-prod-price">
+          ${price}
+      </div>
+      <div className="cart-prod-quantity">
+        <button>-</button>
+        <div className="count">{amount}</div>
+        <button>+</button>
+      </div>
+      <div className="cart-product-total-price">
+          ${price * amount}
+      </div>
+    </>
+
   )
 }
 
